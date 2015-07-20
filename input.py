e638025f11f7e5ac54e8322627416a73564646aa
theffc <fred_ffc@hotmail.com>
2015-07-20 00:28:15 -0300

    Várias mudanças, e a fase de teste começou!

--- input.py
+++ input.py
@@ -1,11 +1,12 @@
-import re
 
+import re
 from collections import namedtuple # nt
 ntAttr = namedtuple("ntAttr", "attr type max_size pos")
 ntTable = namedtuple("Attribute", "type max_size pos")
 ntField = namedtuple("Field", "type max_size")
+from trab import *
 
-text = 'meuBD	 (Aluno)\n\t   Aluno      (name: varchar(80), age: int, birth: varchar(12), id: 	varchar  (' \
+text = '\n\t   Aluno      (name: varchar(80), age: int, birth: varchar(12), id: 	varchar  (' \
        '14))\n\tAluno(Roberto Rodrigues, 21, 05/04/1994, 54a5s4df)\n'
 
 text = re.sub("[ \t\n]", "", text)
@@ -23,7 +24,7 @@ regex2 = re.compile("," + reAttr + reType, re.VERBOSE)
 regexFull = re.compile(reRelation + "+", re.VERBOSE)
 regexRel = re.compile(reRelation, re.VERBOSE)
 
-def check_type (type:str, size:int) -> tuple:
+def check_type (type:str, size:str) -> tuple:
     if not type:
         type, size = None, None
     elif type == "int":
@@ -31,48 +32,8 @@ def check_type (type:str, size:int) -> tuple:
         size = 4
     elif type == "varchar":
         type = "s"
-
+        size = int(size)
     return type, size
-
-
-def treat_table(textRel:str) -> bool:
-    lAttr = [] # [ ( attr_name, type, size, pos ) ]
-    pos = 0
-
-# matches the first part of the relation. E.g: "Relation1(Attribute1[type1(size1)]"
-    match1 = regex1.search(textRel)
-    tableName = match1.group("table")
-    t = match1.group("type")
-    s = match1.group("size")
-    t, s = check_type(t, s)
-    lAttr.append( ntAttr(match1.group("attr"), t, s, pos) )
-    textRel = textRel[match1.end():end]
-
-# matches all attributes and its types, excluding the first.
-    iterAttr = regex2.finditer(textRel)
-    for match2 in iterAttr:
-        pos += 1
-        t = match2.group("type")
-        s = match2.group("size")
-        t, s = check_type(t, s)
-        lAttr.append( ntAttr(match2.group("attr"), t, s, pos) )
-
-# check if the relation is a table declaration: only provides types and sizes, not values. E.g: "Aluno (name: varchar(80), age: int)"
-    if lAttr[0].type:
-        dAttr = dict()
-        vReg = [None] * len(lAttr)
-        regFmt = "="
-        for a in lAttr:
-            dAttr[a.attr] = ntTable(a.type, a.size, a.pos)
-            vReg[a.pos] = ntField(a.type, a.size)
-            regFmt += str(a.size) + a.type
-        #Table(tableName, dAttr, vReg, regFmt) # cria nova tabela
-# otherwise, it's a table insertion providing values. E.g: "Aluno(Roberto Rodrigues, 21)"
-    else:
-        lAttr = [x.attr for x in lAttr]
-        #insert(tableName, lAttr)
-
-    print(lAttr)
 
 def is_valid(text:str) -> bool:
     match = regexFull.fullmatch(text)
@@ -81,6 +42,48 @@ def is_valid(text:str) -> bool:
         return False
     print(match)
     return True
+
+def treat_table(textRel:str) -> bool:
+    lAttr = [] # [ ( attr_name, type, max_size, pos ) ]
+    pos = 0
+# matches the first part of the relation. E.g: "Relation1(Attribute1[type1(size1)]"
+    match1 = regex1.search(textRel)
+    tableName = match1.group("table")
+    t = match1.group("type")
+    s = match1.group("size")
+    print('WOLOLO', s)
+    t, s = check_type(t, s)
+    lAttr.append( ntAttr(match1.group("attr"), t, s, pos) )
+    textRel = textRel[match1.end():end]
+# matches all attributes and its types, excluding the first.
+    iterAttr = regex2.finditer(textRel)
+    for match2 in iterAttr:
+        pos += 1
+        t = match2.group("type")
+        s = match2.group("size")
+        t, s = check_type(t, s)
+        lAttr.append( ntAttr(match2.group("attr"), t, s, pos) )
+# check if the relation is a table declaration: only provides types and sizes, not values. E.g: "Aluno (name: varchar(80), age: int)"
+    if lAttr[0].type:
+    # is a table declaration
+        dAttr = dict()
+        nt_fields = [None] * len(lAttr)
+        regFmt = "="
+        for a in lAttr:
+            dAttr[a.attr] = ntTable(a.type, a.max_size, a.pos)
+            nt_fields[a.pos] = ntField(a.type, a.max_size)
+            regFmt += str(a.max_size) + a.type
+        create_table(tableName, dAttr, nt_fields) # cria nova tabela
+# otherwise, it's a table insertion providing values. E.g: "Aluno(Roberto Rodrigues, 21)"
+    else:
+    # table insertion
+        lAttr = [x.attr for x in lAttr]
+        insert(tableName, lAttr)
+        #insert(tableName, lAttr)
+
+    print(lAttr)
+
+
 
 if not is_valid(text):
     exit()
--- trab.py
+++ trab.py
@@ -1,4 +1,8 @@
 # coding=utf-8
+class Table: pass
+class DirectoryPage: pass
+class RegisterPage: pass
+class Register: pass
 
 from collections import namedtuple # nt()
 RegSlot = namedtuple("Register_Slot", "regStart, regSize")
@@ -11,32 +15,41 @@ import io
 
 dCatalog = dict()
 
-dCatalog["Scheme1"]["Table1"]["Attribute1"] = "int"
-dCatalog["Scheme1"]["Table1"]["Attribute2"] = "varchar(64)"
+# dCatalog["Scheme1"]["Table1"]["Attribute1"] = "int"
+# dCatalog["Scheme1"]["Table1"]["Attribute2"] = "varchar(64)"
 
 PAGE_SIZE = 8192
 QUATRO_GB = 4294967296
 
 
-def my_open(file, offset=0) -> mmap.mmap:
+def my_open(file, offset=0, size=PAGE_SIZE) -> mmap.mmap:
 	if isinstance(file, mmap.mmap):
 	# received an actual mmap object that will be returned
 		if not file.closed:
-			file.flush(offset, PAGE_SIZE)
+			file.flush(offset, size)
 		return file # mmap
 	elif isinstance(file, str):
 	# path to the file
-		file = open(file, mode='r+b', buffering=PAGE_SIZE)
+		file = open(file, mode='r+b', buffering=size)
 	elif isinstance(file, io.FileIO):
 	# file object that was already opened
 		if file.closed:
-			file = open(file.name, mode='r+b', buffering=PAGE_SIZE)
+			file = open(file.name, mode='r+b', buffering=size)
 		else:
 			file.flush()
 	else: # invalid input
 		return None
 
-	return mmap.mmap(file.fileno(), PAGE_SIZE, offset=offset)
+	return mmap.mmap(file.fileno(), size, offset=offset)
+
+
+def create_table(table:str, dAttributes:dict, nt_fields:list):
+	dCatalog[table] = Table(table, dAttributes, nt_fields)
+	open("tables\\"+table, mode='wb', buffering=0)
+	print(dCatalog[table].file)
+
+def insert(table:str, values:list):
+	dCatalog[table].insert(values)
 
 
 class Catalog(object):
@@ -56,9 +69,8 @@ class Scheme(object):
 class Table(object):
 	"""docstring for Table"""
 	def __init__(self, name:str, dAttributes:dict, nt_fields:list):
-		super(Table, self).__init__()
 		self.name = name
-		self.archive = "tables/" + str(name)
+		self.archive = "tables\\" + str(name)
 
 		self.dAttributes = dAttributes # dicionário de características importantes para cada atributo
 		#{ attr_name: nt(pos, type, max_size) , ...}
@@ -73,29 +85,26 @@ class Table(object):
 
 
 	def insert(self, values:list):
-
-		with open(self.archive, mode='r+b', buffering=PAGE_SIZE) as f:
-			dir = DirectoryPage.load(f)
-		# estimates row's size, and provides a formatted string for the struct module
-			structFmt = "="
-			size = 0
-			fields_actual_size=[]
-			for i, v in enumerate(values):
-				if self.nt_fields[i]["type"] is "i":
-					fields_actual_size.append(0)
-					size += 4
-					structFmt += "i"
-				else:
-					max = self.nt_fields[i]["max_size"]
-					n = ( len(v) if len(v) < max  else max )
-					size += n
-					fields_actual_size.append(n)
-					structFmt += "H" + str(n) + "s"
-
-			reg = Register(size=size, fields=values, structFmt=structFmt, fields_actual_size=fields_actual_size)
-
-			page = dir.insert(reg)
-			page.insert(values, reg)
+	# estimates row's size, and provides a formatted string for the struct module
+		structFmt = "="
+		size = 0
+		fields_actual_size=[]
+		for i, v in enumerate(values):
+			if self.nt_fields[i].type is "i":
+				fields_actual_size.append(0)
+				size += 4
+				structFmt += "i"
+			else:
+				max = self.nt_fields[i].max_size
+				n = ( len(v) if len(v) < max  else max )
+				size += n
+				fields_actual_size.append(n)
+				structFmt += "H" + str(n) + 'c'
+		# makes a Register with above variables
+		reg = Register(self, size=size, fields=values, structFmt=structFmt, fields_actual_size=fields_actual_size)
+		# loads the first DirectoryPage of this Table
+		dir = DirectoryPage.load(self)
+		dir.insert(reg)
 
 
 
@@ -109,14 +118,15 @@ class DirectoryPage(object):
 		self.baseAddr = baseAddr
 		self.numOfEntries = numOfEntries
 		self.nextDir = nextDir
-		self.entries = entries
+		if not entries:
+			self.entries = []
 
-		self.table = Table
+		self.table = table
 
 	@staticmethod
-	def load(table:Table, start:int=0) -> DirectoryPage:
+	def load(table:Table, base:int=0) -> __init__:
 		cls = DirectoryPage
-		mm = my_open(table.file, start)
+		mm = my_open(table.file, base)
 
 		base, n, next = struct.unpack_from(cls.dirFmt, mm, PAGE_SIZE-cls.dirSize)
 
@@ -126,18 +136,7 @@ class DirectoryPage(object):
 			entries.append(struct.unpack_from(cls.entryFmt, mm, offset)[0])
 			offset += cls.entrySize
 
-		return cls(base, n, next, entries, file)
-
-	#todo arrumar a criação de uma RegisterPage
-	@staticmethod
-	def create(table:Table, baseAddr:int) -> int:
-		cls = DirectoryPage
-
-		pageNumber = RegisterPage.create()
-		pageSize = RegisterPage.emptySize
-		newDir = cls(table, baseAddr, 1, 0, [pageSize])
-		newDir.save()
-		return newDir.baseAddr
+		return cls(table, base, n, next, entries)
 
 	def save(self):
 		cls = DirectoryPage
@@ -152,27 +151,33 @@ class DirectoryPage(object):
 
 	def insert(self, Reg:Register) -> int:
 		cls = DirectoryPage
-		registerSize = Reg.size
+
+		if not self.entries:
+			# directory page is totally empty
+			return self.new_entry(Reg)
 	# tries to find a page with empty space
 		for i, e in enumerate(self.entries):
-			if e >= registerSize:
+			if e >= Reg.size:
 				pageNumber = self.baseAddr * i
-				page = RegisterPage.load(self.table.file, start=pageNumber)
+				page = RegisterPage.load(self.table, baseAddr=pageNumber)
 				if page.insert(Reg):
+					self.entries[i] -= Reg.size
+					self.save()
 					return page
 				#return self.baseAddr * i
-	#
 		if not self.is_full():
 			# DirectoryPage has room for another page entry
 			return self.new_entry(Reg)
-
-	# no page with empty space in this directory
+	# no page with empty space in this full filled directory
 		if not self.nextDir:
 			# directory's pages ended; should create a new one
-			b = PAGE_SIZE*self.numOfEntries + self.baseAddr
-			self.nextDir = cls.create(file=self.file, baseAddr=b)
-		# going to check if there is empty space in the next directory page
-		dir = DirectoryPage.load(self.file, self.nextDir)
+			base = (PAGE_SIZE * self.numOfEntries) + self.baseAddr
+			self.nextDir = base
+			self.save()
+			newDir = RegisterPage(self.table, base)
+			return newDir.insert(Reg)
+	# going to check if there is empty space in the next directory page
+		dir = DirectoryPage.load(self.table, self.nextDir)
 		return dir.insert(Reg)
 
 	def is_full(self) -> bool:
@@ -182,12 +187,19 @@ class DirectoryPage(object):
 		freeSpace = PAGE_SIZE - usedSpace
 		return (False if freeSpace > cls.entrySize  else True)
 
-	def new_entry(self, Reg:Register):
-		cls = DirectoryPage
-
-		self.entries.append([PAGE_SIZE])
-
-		regPage = RegisterPage(baseAddr=self.baseAddr)
+	def new_entry(self, Reg:Register) -> int:
+		"""
+		Creates a new entry and inserts the Register in the corresponding RegisterPage.
+		:param Reg:
+		:return:
+		"""
+		self.entries.append(PAGE_SIZE - Reg.size)
+		self.numOfEntries += 1
+		base = len(self.entries) * self.baseAddr
+		regPage = RegisterPage(self.table, base)
+		regPage.insert(Reg)
+		self.save()
+		return base
 
 
 
@@ -199,70 +211,71 @@ class RegisterPage(object):
 
 	emptySize = metaSize + slotSize
 
-	def __init__(self, table:Table, numOfSlots:int=0, slots:list=None, baseAddr:int=0, startOfFreeSpace:int=0, registers:list=None):
+	def __init__(self, table, baseAddr:int, numOfSlots:int=0, slots:list=None, startOfFreeSpace:int=0, registers:list=None):
 		self.startOfFreeSpace = startOfFreeSpace
 		self.numOfSlots = numOfSlots
-		self.slots = slots
-
+		self.slots = (slots if slots  else [])
 		self.table = table
 		self.baseAddr = baseAddr
-		self.registers = registers
+		self.registers = (registers if registers  else [])
 
 
 	@staticmethod
-	def load(table:Table, baseAddr:int=0) -> __init__:
+	def load(table:Table, baseAddr:int) -> __init__:
 		cls = RegisterPage
 		mm = my_open(table.file, baseAddr)
 	# loads meta information
-		free, numSlots = struct.unpack_from(cls.metaFmt, mm, PAGE_SIZE-cls.metaSize)
+		offset = PAGE_SIZE - cls.metaSize
+		free, numSlots = struct.unpack_from(cls.metaFmt, mm, offset)
 	# loads slots
 		slots = []
-		offset = 0
 		for _ in range(numSlots):
+			offset -= cls.slotSize
 			a, b = struct.unpack_from(cls.slotFmt, mm, offset)
 			slots.append(RegSlot(a, b))
-			offset += cls.slotSize
 	# for each slot in the page, creates a register
 		registers = []
 		for i_slot, slot in enumerate(slots):
+			if slot.regSize < 0:
+				# there is no register associated with that slot
+				registers.append(None)
+				continue
 			rid = Rid(baseAddr, i_slot)
-			reg = Register.load(table.file, rid, slot)
-			Register.load
+			reg = Register.load(table, rid, mm[slot.regStart:slot.regSize])
 			registers.append(reg)
 		# creates a RegisterPage and returns that object
-		return RegisterPage(table, numOfSlots=numSlots, slots=slots, baseAddr=baseAddr, startOfFreeSpace=freeSpace, registers=registers)
+		return RegisterPage(table, baseAddr, numOfSlots=numSlots, slots=slots, startOfFreeSpace=free, registers=registers)
 	#END load()
 
-	@staticmethod
-	def create(file:io.IOBase, baseAddr):
-		page = RegisterPage(file=file, baseAddr=baseAddr)
-		page.save()
-
-
-		return 1
 
 #todo talvez haverá a necessidade de salvar também a basePage.
+	# aparentemente não precisa pq nas principais operações já tem um baseAddr
 	def save(self):
 		cls = RegisterPage
 		mm = my_open(self.table.file, self.baseAddr)
 
-		struct.pack_into(cls.metaFmt, mm, 0, self.startOfFreeSpace, self.numOfSlots)
+		offset = PAGE_SIZE - cls.metaSize
+		struct.pack_into(cls.metaFmt, mm, offset, self.startOfFreeSpace, self.numOfSlots)
 
 		for i, slot in enumerate(self.slots):
 		# this first part stores slots
-			offset = (i * cls.slotSize) + cls.metaSize
+			offset -= cls.slotSize
 			struct.pack_into(cls.slotFmt, mm, offset, *slot)
 		# this part stores registers
 			if slot.regSize < 0:
 				# there is no register associated with that slot
 				continue
 			reg = self.registers[i]
-
+			mm[slot.regStart:slot.regSize] = reg.save()
 	#END save()
 
 
-	def insert(self, Reg:Register):
-		cls = RegisterPage
+	def insert(self, Reg:Register) -> bool:
+		if not self.slots:
+			# this page its completely empty
+			self.new_slot(Reg)
+			self.save()
+			return True
 	# checks if there's an unused slot that can hold the register
 		for i, s in enumerate(self.slots):
 			if s.regSize < 0 and s.regSize >= Reg.size:
@@ -270,58 +283,67 @@ class RegisterPage(object):
 				s.regSize *= -1
 				# stores in which page and slot the Register can be found
 				Reg.rid = Rid(self.baseAddr, i)
+				self.registers[i] = Reg
+				self.save()
 				return True
 	# checks if there's enough space in this page to store the new register and slot
-		spaceRequired = Reg.size + cls.slotSize
+		if not self.has_space(Reg.size):
+			return False
+	# creates a new slot and stores the register
+		self.new_slot(Reg)
+		self.save()
+		return True
+	#END insert()
+
+	def has_space(self, size:int) -> bool:
+		cls = RegisterPage
+		spaceRequired = size + cls.slotSize
 		dirSpace = cls.metaSize + (self.numOfSlots * cls.slotSize)
 		spaceGranted = PAGE_SIZE - self.startOfFreeSpace - dirSpace
 		if spaceRequired > spaceGranted:
 			# can't insert the register in this page
 			return False
-	# creates a new slot and stores the register
-		newSlot = RegSlot(self.startOfFreeSpace, Reg.size)
-		self.slots.append(newSlot)
-		self.numOfSlots += 1
-		self.startOfFreeSpace += Reg.size
+		else: # has space
+			return True
+
+	def new_slot(self, Reg:Register):
 		# stores in which page and slot the Register can be found
 		Reg.rid = Rid(self.baseAddr, self.numOfSlots-1)
-		return True
-	#END insert()
+		newSlot = RegSlot(self.startOfFreeSpace, Reg.size)
+		self.slots.append(newSlot)
+		self.registers.append(Reg)
+		self.numOfSlots += 1
+		self.startOfFreeSpace += Reg.size
+
 
 
 class Register(object):
 	"""docstring for Register"""
-	
-	def __init__(self, table:Table, size:int=0, fields:list=[], fields_actual_size:list=[], structFmt:str="",  rid:Rid=Rid(0,0)):
-		super(Register, self).__init__()
+
+	def __init__(self, table:Table, size:int=0, fields:list=None, fields_actual_size:list=None, structFmt:str="",  rid:Rid=Rid(0,0)):
 		self.rid = rid
 		self.size = size
-		
-		self.fields = fields
-		
-		self.fields_actual_size = fields_actual_size
+		self.fields = (fields if fields  else [])
+		self.fields_actual_size = (fields_actual_size if fields_actual_size  else [])
 		self.structFmt = structFmt
 		self.table = table
 		#self.space =
 
 	@staticmethod
-	def load(table:Table, rid:Rid, slot:RegSlot) -> __init__:
-		if slot.regSize < 0:
-			# there is no register associated with that slot
-			return None
-		mm = my_open(table.file, rid.page)
-	#for each field in the Register, updates the following variables
+	def load(table:Table, rid:Rid, mm:mmap.mmap) -> __init__:
+	# for each field in the Register, updates the following variables
 		finalFmt = "=" # used with struct module
-		finalSize = 0 	
-		fields, fields_actual_size = [], []
-		offset = slot.regStart
+		finalSize = 0
+		fields = []
+		fields_actual_size = []
+		offset = 0
 		for field in table.nt_fields:
-			if field.type == 's': # string type
+			if field.type == 'c': # string type
 			# needs to load fields size before loading the actual fields value
 				size = struct.unpack_from('H', mm, offset)
 				fields_actual_size.append(size)
 				offset += size
-				fmt = str(size) + 's'
+				fmt = str(size) + 'c'
 				finalFmt += 'H'
 			else: # integer type
 				fmt = 'i'
@@ -332,16 +354,18 @@ class Register(object):
 			fields.append(struct.unpack_from(fmt, mm, offset))
 			offset += field.size
 		# creates a Register and returns it
-		reg = Register(size=finalSize, fields=fields, fields_actual_size=fields_actual_size, structFmt=finalFmt, rid=rid)
-		return reg
+		return Register(table, size=finalSize, fields=fields, fields_actual_size=fields_actual_size, structFmt=finalFmt, rid=rid)
 
-#todo olhar a parte do slot nesse save, talvez precisará receber ele como parâmetro
-	def save(self):
+#todo ver como transformar uma string para bytes, pq o modulo struct só aceita bytes
+	def save(self) -> bytes:
 		""" since registers can have strings of mutable size, they need to store how much space is allocated for each string. Thus, we will get fields and fields size into "values"."""
-		mm = my_open(self.table.file, self.rid.page)
+		print(self.rid)
 		values = []
 		for f in self.fields:
 			try: int(f)
 			except: values.append(len(f))
 			values.append(f)
-		struct.pack_into(self.structFmt, mm, slot.regStart, *values)
+		print(values)
+		print(self.structFmt)
+		return struct.pack(self.structFmt, *values)
+
